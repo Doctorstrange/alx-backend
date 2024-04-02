0979b01b5930399ed32c58793bf9d08a1b98374e
doctorstrange <doctorstrange@gmail.com>
2024-04-02 01:46:57 +0000

    0. Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Return a tuple of size two containing a start index and an end index
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of size two containing a start index and an end index
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
