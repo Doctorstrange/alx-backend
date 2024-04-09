2ee6a76b9d205e0c0231c954b1b027f8d1e96810
doctorstrange <doctorstrange@gmail.com>
2024-04-09 22:30:25 +0000

    5. Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -21,8 +21,33 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """get user with user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """before request
+    """
+
+    g.user = get_user()
+
+
 @babel.localeselector
-def get_locale()-> str:
+def get_locale():
     """ determine the best match with our supported languages.
     Returns: best match
     """
