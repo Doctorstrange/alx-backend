ca93660067dc26d8ce8185b9f57016f19a197cf0
doctorstrange <doctorstrange@gmail.com>
2024-04-09 22:41:10 +0000

    5. Mock logging in

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -9,6 +9,7 @@ from flask_babel import Babel
 class Config:
     '''Config class'''
 
+    DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
